08205334d75a70676bd079a8a2094555e4323807
geri1701 <geri@sdf.org>
2023-04-07 01:15:05 +0200

    Read contents from config into hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
 fn main() {
-    println!("Hello, world!");
+    let mut config = File::open("~/.realify.conf").expect("Failed to open file");
+    let mut assign = HashMap::<String, String>::new();
+    let mut contents = String::new();
+    config
+        .read_to_string(&mut contents)
+        .expect("Failed to read file");
+    for line in contents.lines() {
+        if let Some((k, v)) = line.split_once(" ") {
+            assign.insert(k.into(), v.into());
+        }
+    }
 }
