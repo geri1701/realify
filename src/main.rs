27e31afcc763c051fbac47f41064f938683d3597
geri1701 <geri@sdf.org>
2023-04-07 23:03:30 +0200

    Add clap to dependencies

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ use std::fs::File;
 use std::io::prelude::*;
 fn main() {
     let mut config = File::open("~/.realify.conf").expect("Failed to open file");
-    let mut assign = HashMap::<String, String>::new();
+    let mut table = HashMap::<String, String>::new();
     let mut contents = String::new();
     config
         .read_to_string(&mut contents)
         .expect("Failed to read file");
     for line in contents.lines() {
         if let Some((k, v)) = line.split_once(" ") {
-            assign.insert(k.into(), v.into());
+            table.insert(k.into(), v.into());
         }
     }
 }
