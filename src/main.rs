747a694adaa87d8f56b01632bbbe4a8d505217f2
geri1701 <geri@sdf.org>
2023-04-09 01:35:25 +0200

    Extract the logic

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
+
 #[derive(Parser)]
 #[command(name = "Realify")]
 #[command(author = "Gerhard S. <geri@sdf.org>")]
@@ -13,22 +14,33 @@ struct Cli {
     #[arg(long)]
     name: String,
 }
-fn main() {
-    let cli = Cli::parse();
-    let mut config = File::open(cli.config).expect("Failed to open file");
+
+fn realify(config: &str, name: &str) -> Result<String, String> {
+    let mut config_file = File::open(config).map_err(|e| e.to_string())?;
     let mut table = HashMap::<String, String>::new();
     let mut contents = String::new();
-    config
+    config_file
         .read_to_string(&mut contents)
-        .expect("Failed to read file");
+        .map_err(|e| e.to_string())?;
     for line in contents.lines() {
         if let Some((k, v)) = line.split_once(" ") {
             table.insert(k.into(), v.into());
         }
     }
-    if table.contains_key(&cli.name) {
-        println!("{}", table.get(&cli.name).unwrap());
+    if let Some(value) = table.get(name) {
+        Ok(value.to_string())
     } else {
-        eprintln!("Error: Your config doesn't contain the key {}", cli.name);
+        Err(format!(
+            "Error: Your config doesn't contain the key {}",
+            name
+        ))
+    }
+}
+
+fn main() {
+    let cli = Cli::parse();
+    match realify(&cli.config, &cli.name) {
+        Ok(value) => println!("{}", value),
+        Err(err) => eprintln!("{}", err),
     }
 }
