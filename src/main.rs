f76cbe682c13404d4ca32d82febe564d5eaa1007
geri1701 <geri@sdf.org>
2023-04-08 22:54:10 +0200

    Add clap struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use clap::Parser;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
+#[derive(Parser)]
+#[command(name = "Realify")]
+#[command(author = "Gerhard S. <geri@sdf.org>")]
+#[command(version = "0.1.0")]
+#[command(about = "Assigns a name to a key.", long_about = None)]
+struct Cli {
+    #[arg(long)]
+    config: String,
+    #[arg(long)]
+    name: String,
+}
 fn main() {
-    let mut config = File::open("~/.realify.conf").expect("Failed to open file");
+    let cli = Cli::parse();
+    let mut config = File::open(cli.config).expect("Failed to open file");
     let mut table = HashMap::<String, String>::new();
     let mut contents = String::new();
     config
